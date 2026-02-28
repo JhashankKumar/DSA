9c913c3b14c9d4b9ffc0eb6d85687e393a9af0e6
Jhashank <Jhashank@nuevesolutions.in>
2026-02-28 11:52:38 +0530

    frist NQT Problem

---
+++ NQT/Arrays/Average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int array_size = scanner.nextInt();
+        int average_list[] = new int[array_size];
+        for(int i = 0;i<array_size;i++){
+            average_list[i] = scanner.nextInt();
+        }
+        scanner.close();
+        float average = average(average_list, array_size);
+        System.out.println(average);
+    }
+
+    private static float average(int array[], int array_size){
+    int sum = 0;
+    for(int value: array){
+        sum += value;
+    }
+    float average = (float)sum/array_size;
+    return average;
+    }
+}
