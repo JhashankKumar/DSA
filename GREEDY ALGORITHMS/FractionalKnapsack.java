831cc6c400166640e6ff29b0a5728d65275c870b
jhashank <jhashank@nuevesolutions.in>
2026-04-14 01:07:53 +0000

    enhanced fractional knapsack and chain length pair

--- GREEDY ALGORITHMS/FractionalKnapsack.java
+++ GREEDY ALGORITHMS/FractionalKnapsack.java
@@ -1,3 +1,52 @@
+/*
+Given two arrays, val[] and wt[], representing the values and weights of item respectively, 
+and an integer capacity representing the maximum weight a knapsack can hold, we have to 
+determine the maximum total value that can be achieved by putting the items in the knapsack 
+without exceeding its capacity.
+Items can also be taken in fractional parts if required.
+
+Examples:
+
+Input: val[] = [60, 100, 120], wt[] = [10, 20, 30], capacity = 50
+Output: 240 
+Explanation: We will take the items of weight 10kg and 20kg and 2/3 fraction of 30kg. 
+Hence total value will be 60 + 100 + (2/3) * 120 = 240.
+
+Input: val[] = [500], wt[] = [30], capacity = 10
+Output: 166.667
+*/
+
+/*
+Why naive approach will fail?
+Case 1: Picking the items with smaller weights first
+Example: val[] = [10, 10, 10, 100], wt[] = [10, 10, 10, 30], capacity = 30
+
+If we start picking smaller weights, we can pick the three items of weight 10 → total value = 10+10+10 = 30.
+But the optimal choice is to take the last item (value 100, weight 30) → total value = 100.
+So, choosing smaller weights first fails here.
+
+Case 2: Picking items with larger value first
+Example: val[]= [10, 10, 10, 20], wt[] = [10, 10, 10, 30], capacity = 30
+
+If we start picking higher values, we might choose the last item (value 20, weight 30) → total value = 20.
+But the better choice is to take the three items of weight 10 each → total value = 10+10+10 = 30.
+So, choosing higher values first also fails.
+
+[Approach] Selecting Items by value/weight Ratio - O(nlogn) Time and O(n) Space
+The idea is to always pick items greedily based on their value-to-weight ratio. 
+Take the item with the highest ratio first, then the next highest, and so on, until the knapsack is full. 
+If any item doesn’t fully fit, then take its fractional part according to the remaining capacity.
+
+Steps to solve the problem:
+
+Calculate the ratio (value/weight) for each item.
+Sort all the items in decreasing order of the ratio.
+Iterate through items:
+if the current item fully fits, add its full value and decrease capacity otherwise, 
+take the fractional part that fits and add proportional value.
+Stop once the capacity becomes zero.
+
+*/
 import java.util.*;
 public class FractionalKnapsack {
     public static void main(String[] args) {
--- GREEDY ALGORITHMS/maxLenChainPair.java
+++ GREEDY ALGORITHMS/maxLenChainPair.java
@@ -1,15 +1,69 @@
+/*
+You are given n pairs of numbers. In every pair, the first number is always smaller than 
+the second number. A pair (c, d) can follow another pair (a, b) if b < c. 
+Chain of pairs can be formed in this fashion. Find the longest chain which can be formed 
+from a given set of pairs.
+
+For example, if the given pairs are {{5, 24}, {39, 60}, {15, 28}, {27, 40}, {50, 90} }, 
+then the longest chain that can be formed is of length 3, and the chain is {{5, 24}, {27, 40}, {50, 90}}
+*/
+/*
+[Approach] Sort the pairs based on the second element and then apply a greedy approach to 
+find the longest chain.
+Steps to solve the problem:
+Sort the pairs in increasing order of their second element.
+1. Initialize a variable to keep track of the length of the longest chain (chainlen) and 
+set it to 1 (since at least one pair can always form a chain).
+2. Initialize a variable (chainEnd) to keep track of the end of the last selected pair, 
+and set it to the second element of the first pair.
+3. Iterate through the sorted pairs starting from the second pair:
+4. If the first element of the current pair is greater than the chainEnd, it means we can extend 
+the chain by including this pair. In this case, increment the chainlen and update the chainEnd to 
+the second element of the current pair.
+5. Continue this process until you have iterated through all the pairs.    
+*/
 import java.util.*;
 public class maxLenChainPair {
     public static void main(String[] args) {
         int pairs[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
 
+        solveUsingArray(pairs);
+        SolveUsingArrayList(pairs);
+    }
+
+    public static void solveUsingArray(int pairs[][]){
+        //sort the pairs on the basis of their second element in ascending order
         Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
         int chainlen=1;
         int chainEnd = pairs[0][1];//last selected pair end // chain end
+        System.out.print("("+pairs[0][0]+","+pairs[0][1]+") ");
         for(int i=1;i<pairs.length;i++){
             if(pairs[i][0]>chainEnd){
                 chainlen++;
+                System.out.print("("+pairs[i][0]+","+pairs[i][1]+") ");
                 chainEnd=pairs[i][1];
+            }
+        }
+        System.out.println(chainlen);
+    }
+
+    public static void SolveUsingArrayList(int pairs[][]){
+        //sort the pairs on the basis of their second element in ascending order
+        //sort in arraylist is done by using comparator and lambda function
+        List<int[]> pairList = new ArrayList<>();
+        for(int[] pair : pairs){
+            pairList.add(pair);
+        }
+        // pairList.sort(Comparator.comparingDouble(o->o[1]));
+        Collections.sort(pairList,Comparator.comparingDouble(o->o[1]));
+        int chainlen=1;
+        int chainEnd = pairList.get(0)[1];//last selected pair end // chain end
+        System.out.print("("+pairList.get(0)[0]+","+pairList.get(0)[1]+") ");
+        for(int i=1;i<pairList.size();i++){
+            if(pairList.get(i)[0]>chainEnd){
+                chainlen++;
+                System.out.print("("+pairList.get(i)[0]+","+pairList.get(i)[1]+") ");
+                chainEnd=pairList.get(i)[1];
             }
         }
         System.out.println(chainlen);
