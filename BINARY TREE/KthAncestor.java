c533ef4cbce50beb3d87627784e9ebdbd8635227
jhashank <jhashank@nuevesolutions.in>
2026-04-21 00:55:26 +0000

    Greedy methods

--- BINARY TREE/KthAncestor.java
+++ BINARY TREE/KthAncestor.java
@@ -1,3 +1,10 @@
+//Kth ancestor of a node in binary tree
+/*
+Given a binary tree in which nodes are numbered from 1 to n. Given a node and a positive integer K. 
+We have to print the Kth ancestor of the given node in the binary tree. If there does not exist any 
+such ancestor then print -1.
+For example in the below given binary tree, the 2nd ancestor of 5 is 1. 3rd ancestor of node 5 will be -1. 
+*/
 public class KthAncestor {
     static class Node{
         int data;
