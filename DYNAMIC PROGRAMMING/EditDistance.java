43bb61e4e151a50139d65e54ad3cdcc17e4db982
jhashank <jhashank@nuevesolutions.in>
2026-04-26 15:59:19 +0000

    Add detailed comments and examples to EditDistance class for better understanding of edit operations

--- DYNAMIC PROGRAMMING/EditDistance.java
+++ DYNAMIC PROGRAMMING/EditDistance.java
@@ -1,3 +1,26 @@
+/*
+Given two strings s1 and s2 and below operations that can be performed on s1. 
+The task is to find the minimum number of edits (operations) to convert 's1' into 's2'.  
+
+Insert: Insert any character before or after any index of s1
+Remove: Remove a character of s1
+Replace: Replace a character at any index of s1 with some other character.
+Note: All of the above operations are of equal cost. 
+
+Examples: 
+
+Input: s1 = "geek", s2 = "gesek"
+Output: 1
+Explanation: We can convert s1 into s2 by inserting an 's' between two consecutive 'e' in s1.
+
+Input: s1 = "gfg", s2 = "gfg"
+Output: 0
+Explanation: Both strings are same.
+
+Input: s1 = "abcd", s2 = "bcfe"
+Output: 3
+Explanation: We can convert s1 into s2 by removing 'a', replacing 'd' with 'f' and inserting 'e' at the end. 
+*/
 public class EditDistance {
     public static int editDistance(String str1, String str2){
         int n = str1.length();
