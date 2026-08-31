4c865e40a13fce18119f49b69b074adcdfff384b
jhashank <jhashank@nuevesolutions.in>
2026-08-31 16:14:27 +0000

    Add iterative approach to find minimum and maximum elements in an array

--- DSA Basics/Data Structures/ARRAYS/Python/min_max_in_array.py
+++ DSA Basics/Data Structures/ARRAYS/Python/min_max_in_array.py
@@ -14,6 +14,8 @@ Explanation: The minimum element is 3, and the maximum element is 35.
 """
 
 """
+Approach 1:
+
 [Naive Approach] By Sorting the array - O(n log n) Time and O(1) Space
 The idea is to firstly sort the array in ascending order.
 Once the array is sorted, the first element of the array will be the minimum element and the 
@@ -33,11 +35,34 @@ def min_max_sorting(arr):
     # The first element is the minimum and the last element is the maximum
     return [arr[0], arr[-1]]
 
+"""
+Approach 2:
+
+[Better Approach I] Iterating the array - O(n) Time and O(1) Space
+
+"""
+
+def min_max_iterating(arr):
+    # Initialize min and max with the first element of the array
+    min_elem = arr[0]
+    max_elem = arr[0]
+    
+    # Iterate through the array to find min and max
+    for num in arr:
+        if num < min_elem:
+            min_elem = num
+        elif num > max_elem:
+            max_elem = num
+            
+    return [min_elem, max_elem]
+
 
 if __name__ == "__main__":
     # Test cases
     arr1 = [3, 5, 4, 1, 9]
     print(min_max_sorting(arr1))  # Output: [1, 9]
+    print(min_max_iterating(arr1))  # Output: [1, 9]
 
     arr2 = [22, 14, 8, 17, 35, 3]
     print(min_max_sorting(arr2))  # Output: [3, 35]
+    print(min_max_iterating(arr2))  # Output: [3, 35]
